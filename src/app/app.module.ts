3657caa06b6346d95603e281cc392967c6f24442
Joshua Proffitt <joshua.m.proffitt@gmail.com>
2019-07-24 15:42:25 -0400

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule, MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    [NoopAnimationsModule],
+    BrowserAnimationsModule,
+    [MatButtonModule, MatCheckboxModule],
+    MatToolbarModule
   ],
+  exports: [MatButtonModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class MyOwnCustomMaterialModule { }
+export class PizzaPartyAppModule { }
